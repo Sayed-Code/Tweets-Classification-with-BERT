70b0a56240971883d969bf162790e308f0a126b6
mouhamadalsayed810 <mouhamadalsayed810@gmail.com>
2021-10-06 20:58:36 +0200

    chng: fixed path

--- xgb_rf/path_to_data.py
+++ xgb_rf/path_to_data.py
@@ -1,24 +1,17 @@
+from bert.utils import get_project_root
 
-PATH_TRAINING_2020 = '/home/firm/Desktop/personal/xgb_klagenfurt/data/2020/' \
-                '2020_full_German/exp1/train.xlsx'
-PATH_VALIDATION_2020 = '/home/firm/Desktop/personal/xgb_klagenfurt/data/2020/' \
-               '2020_full_German/exp1/validation.xlsx'
+ROOT = str(get_project_root())
+PATH_TRAINING_2020 = ROOT + '/data/2020/2020_full_German/exp1/train.xlsx'
+PATH_VALIDATION_2020 = ROOT + '/data/2020/2020_full_German/exp1/validation.xlsx'
 
-PATH_TRAINING_2019 = '/home/firm/Desktop/personal/xgb_klagenfurt/data/2019/' \
-                     '2019_English/train.xlsx'
-PATH_VALIDATION_2019 = '/home/firm/Desktop/personal/xgb_klagenfurt/data/2019/' \
-                    '2019_English/validation.xlsx'
+PATH_TRAINING_2019 = ROOT + '/data/2019/2019_English/train.xlsx'
+PATH_VALIDATION_2019 = ROOT + '/data/2019/2019_English/validation.xlsx'
 
-PATH_TRAINING_2019_2020 = '/home/firm/Desktop/personal/xgb_klagenfurt/data/' \
-                          '2019_2020/english/train.xlsx'
-PATH_VALIDATION_2019_2020 = '/home/firm/Desktop/personal/xgb_klagenfurt/data/' \
-                         '2019_2020/english/validation.xlsx'
+PATH_TRAINING_2019_2020 = ROOT + '/data/2019_2020/english/train.xlsx'
+PATH_VALIDATION_2019_2020 = ROOT + '/data/2019_2020/english/validation.xlsx'
 
-PATH_TESTING_2019_2020 = '/home/firm/Desktop/personal/xgb_klagenfurt/data/' \
-                         '2019_2020/english/test.xlsx'
-PATH_TESTING_2020 = '/home/firm/Desktop/personal/xgb_klagenfurt/data/2020/' \
-               '2020_full_German/exp1/test.xlsx'
-PATH_TESTING_2019 = '/home/firm/Desktop/personal/xgb_klagenfurt/data/2019/' \
-                    '2019_English/test.xlsx'
+PATH_TESTING_2019_2020 = ROOT + '/data/2019_2020/english/test.xlsx'
+PATH_TESTING_2020 = ROOT + '/data/2020/2020_full_German/exp1/test.xlsx'
+PATH_TESTING_2019 = ROOT + '/data/2019/2019_English/test.xlsx'
 
-PATH_SAVED_MODELS = '/home/firm/Desktop/personal/xgb_klagenfurt/saved_models'
+PATH_SAVED_MODELS = ROOT + '/saved_models'
